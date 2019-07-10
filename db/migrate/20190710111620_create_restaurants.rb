e414dc378334e48624e6878e8f60ccdb513ccc2e
Charles Shorter <cshorter.mc@gmail.com>
2019-07-10 14:38:35 +0200

    rails new

---
+++ db/migrate/20190710111620_create_restaurants.rb
@@ -0,0 +1,12 @@
+class CreateRestaurants < ActiveRecord::Migration[5.2]
+  def change
+    create_table :restaurants do |t|
+      t.string :name
+      t.string :address
+      t.integer :phone
+      t.string :category
+
+      t.timestamps
+    end
+  end
+end
